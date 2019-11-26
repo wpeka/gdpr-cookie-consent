9a3a85b40ca0368698585d4d1f85401e70180caa
ratnesh-kadam <ratnesh.kadam@hbwsl.com>
2019-11-26 11:16:47 +0530

    Fix - Fatal error if Analytics SDK is included in two or more plugins installed on same WP instance (#15)
    * Feature - added feedback form on plugin deactivation
    
    * Feature - Anonymous Feedback
    
    * Feature - added feedback form on plugin deactivation (#9)
    
    * Feature - added feedback form on plugin deactivation
    
    * Feature - Anonymous Feedback
    
    * Fix - Updated Analytics SDK, feedback data is sent to the mixpanel each time
    
    * Feedback (#11)
    
    * Feature - added feedback form on plugin deactivation
    
    * Feature - Anonymous Feedback
    
    * Fix - Updated Analytics SDK, feedback data is sent to the mixpanel each time
    
    * Feature - Added feedback form on plugin deactivation
    
    * Fix - Read more link URL is not reflected in  admin dashboard
    
    * Release v1.7.1 - updated version number and changelog
    
    * Fix - fatal error if analytics sdk is included in two or more plugins
    
    * Release v1.7.2 - updated version number and changlog

--- analytics/start.php
+++ analytics/start.php
@@ -25,39 +25,40 @@ if ( ! class_exists( 'Analytics' ) ) {
 	if ( ! defined( 'WP_STAT__SDK_VERSION' ) ) {
 		define( 'WP_STAT__SDK_VERSION', $this_sdk_version );
 	}
-}
-// Load SDK files.
-require_once dirname( __FILE__ ) . '/require.php';
 
-/**
- * Dynamic Init.
- *
- * @param array <string,string> $module Plugin or Theme details.
- *
- * @return Analytics
- */
-function ras_dynamic_init( $module ) {
-	if ( ! isset( $module['plugin_basename'] ) ) {
-		$plugin_basename = '';
-	} else {
-		$plugin_basename = $module['plugin_basename'];
+	// Load SDK files.
+	require_once dirname( __FILE__ ) . '/require.php';
+
+	/**
+	 * Dynamic Init.
+	 *
+	 * @param array <string,string> $module Plugin or Theme details.
+	 *
+	 * @return Analytics
+	 */
+	function ras_dynamic_init( $module ) {
+		if ( ! isset( $module['plugin_basename'] ) ) {
+			$plugin_basename = '';
+		} else {
+			$plugin_basename = $module['plugin_basename'];
+		}
+		$ra = Analytics::instance( $module['id'], $module['slug'], $module['product_name'], $module['version'], $module['module_type'], $plugin_basename );
+		$ra->dynamic_init( $module );
+
+		return $ra;
 	}
-	$ra = Analytics::instance( $module['id'], $module['slug'], $module['product_name'], $module['version'], $module['module_type'], $plugin_basename );
-	$ra->dynamic_init( $module );
 
-	return $ra;
-}
-
-/**
- * Quick shortcut to get Analytics for specified plugin.
- * Used by various templates.
- *
- * @param number $module_id Module Id.
- * @param string $slug Slug.
- * @param string $product_name Product Name.
- * @param string $version Product Version.
- * @param string $module_type Module type.
- */
-function analytics( $module_id, $slug, $product_name, $version, $module_type ) {
-	return Analytics::instance( $module_id, $slug, $product_name, $version, $module_type );
+	/**
+	 * Quick shortcut to get Analytics for specified plugin.
+	 * Used by various templates.
+	 *
+	 * @param number $module_id Module Id.
+	 * @param string $slug Slug.
+	 * @param string $product_name Product Name.
+	 * @param string $version Product Version.
+	 * @param string $module_type Module type.
+	 */
+	function analytics( $module_id, $slug, $product_name, $version, $module_type ) {
+		return Analytics::instance( $module_id, $slug, $product_name, $version, $module_type );
+	}
 }
