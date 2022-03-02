441d567c526e1da139c23d0e91048a81064315a2
sanket <sanket@3n19m4.local>
2022-03-02 14:19:07 +0530

    vue components excluded from phpcs

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -2,7 +2,7 @@
 
 if [[ ${RUN_PHPCS} == 1 ]]; then
 	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
-	IGNORE="webpack.config.js,tests/cli/,includes/libraries/,includes/api/legacy/,analytics/,vendor/,node_modules/,languages/,public/js/,public/css/,admin/js/,admin/css/"
+	IGNORE="webpack.config.js,src/vue-components/,*.js,tests/cli/,includes/libraries/,includes/api/legacy/,analytics/,vendor/,node_modules/,languages/,public/js/,public/css/,admin/js/,admin/css/"
 
 
 	echo "Running Code Sniffer."
