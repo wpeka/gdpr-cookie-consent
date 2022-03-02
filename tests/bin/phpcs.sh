5fb08bcf4db47a351ab19a7990ba9d9acc5c9834
sanket <sanket@3n19m4.local>
2022-03-02 14:10:03 +0530

    Changes related to code sniffer in github actions

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -2,10 +2,10 @@
 
 if [[ ${RUN_PHPCS} == 1 ]]; then
 	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
-	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+	IGNORE="webpack.config.js,tests/cli/,includes/libraries/,includes/api/legacy/,analytics/,vendor/,node_modules/,languages/,public/js/,public/css/,admin/js/,admin/css/"
 
 
 	echo "Running Code Sniffer."
-	vendor/bin/phpcs --standard=phpcs.xml --ignore=$IGNORE --encoding=utf-8 -s -n -p
+	vendor/bin/phpcs --standard=WordPress --ignore=$IGNORE --encoding=utf-8 -s -n -p /home/runner/work/gdpr-cookie-consent/gdpr-cookie-consent/
 
 fi
