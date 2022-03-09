10df981cc7cda71a2482dfa177a99eeb9afd0367
virtualrst <rishabhtiwari821@gmail.com>
2022-03-09 14:57:36 +0530

    test cases for policy data

--- tests/modules/test-class-gdpr-cookie-consent-policy-data.php
+++ tests/modules/test-class-gdpr-cookie-consent-policy-data.php
@@ -48,21 +48,27 @@ class Test_Gdpr_Cookie_Consent_Policy_Data extends WP_UnitTestCase {
 		}
 		self::$plugin_name         = 'gdpr_cookie_consent';
 		self::$gdpr_cookie_consent = new Gdpr_Cookie_Consent_Policy_Data();
+		// global $user_ID;
+		// $new_post = array(
+		// 'post_title' => 'My New Post',
+		// 'post_content' => 'Lorem ipsum dolor sit amet...',
+		// 'post_status' => 'publish',
+		// 'post_date' => date('2022-08-08 22:22:22'),
+		// 'post_author' => $user_ID,
+		// 'post_type' => 'gdpr-cookie-consent',
+		// 'post_category' => array(0)
+		// );
+		// $post_id = wp_insert_post($new_post);
 	}
 
 	/**
 	 * Test for construct function
 	 */
 	public function test__construct() {
-		wp_set_current_user(
-			self::factory()->user->create(
-				array(
-					'role' => 'administrator',
-				)
-			)
-		);
+		set_current_screen( 'post-new.php' );
 		$obj = new Gdpr_Cookie_Consent_Policy_Data();
 		$this->assertTrue( $obj instanceof Gdpr_Cookie_Consent_Policy_Data );
+		// $this->assertTrue(is_admin());
 	}
 	/**
 	 * Test for gdpr_register_custom_post_type function.
@@ -78,6 +84,21 @@ class Test_Gdpr_Cookie_Consent_Policy_Data extends WP_UnitTestCase {
 		self::$gdpr_cookie_consent->gdpr_add_meta_box();
 		$this->assertTrue( true );
 	}
+	/**
+	 * Test for gdpr_metaboc_policies_links.
+	 */
+	// public function test_gdpr_metabox_policies_links() {
+	// 	self::$gdpr_cookie_consent->gdpr_metabox_policies_links();
+	// 	$this->assertTrue( true );
+	// }
+	/**
+	 * Test for gdpr_process_csv_export_policies function
+	 */
+	// public function test_gdpr_process_csv_export_policies() {
+	// 	self::$gdpr_cookie_consent->gdpr_process_csv_export_policies();
+	// 	$this->assertTrue( true );
+	// }
 }
+
 
 
