38522f5aa9b696ac1229c5b37065397c456a0726
omendra rajput <omendra.rajput@hbwsl.com>
2023-11-15 15:41:47 +0530

    Added Changelog v2.3.8

--- includes/class-gdpr-cookie-consent.php
+++ includes/class-gdpr-cookie-consent.php
@@ -78,7 +78,7 @@ class Gdpr_Cookie_Consent {
 		if ( defined( 'GDPR_COOKIE_CONSENT_VERSION' ) ) {
 			$this->version = GDPR_COOKIE_CONSENT_VERSION;
 		} else {
-			$this->version = '2.3.7';
+			$this->version = '2.3.8';
 		}
 		$this->plugin_name = 'gdpr-cookie-consent';
 
