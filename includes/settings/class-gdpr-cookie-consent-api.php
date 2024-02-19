7ee4aae546513261eab54d799a4e805d3411d02f
omendra rajput <omendra.rajput@hbwsl.com>
2024-02-19 17:38:13 +0530

    fixed phpcs errors in api framework

--- includes/settings/class-gdpr-cookie-consent-api.php
+++ includes/settings/class-gdpr-cookie-consent-api.php
@@ -55,20 +55,20 @@ class GDPR_Cookie_Consent_Api extends WP_REST_Controller {
 		);
 	}
 
-    /**
+	/**
 	 * Get a collection of items.
 	 *
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return WP_Error|WP_REST_Response
 	 */
 	public function get_items( $request ) {
-        require_once GDPR_COOKIE_CONSENT_PLUGIN_PATH . 'includes/settings/class-gdpr-cookie-consent-settings.php';
+		require_once GDPR_COOKIE_CONSENT_PLUGIN_PATH . 'includes/settings/class-gdpr-cookie-consent-settings.php';
 		$object = new GDPR_Cookie_Consent_Settings();
 		$data   = $object->get();
 		return rest_ensure_response( $data );
 	}
 
-    /**
+	/**
 	 * Check if a given request has access to read items.
 	 *
 	 * @param  WP_REST_Request $request Full details about the request.
@@ -86,7 +86,7 @@ class GDPR_Cookie_Consent_Api extends WP_REST_Controller {
 		$token            = str_replace( 'Bearer ', '', $authorization );
 		$request_platform = $request->get_param( 'platform' );
 
-		if ( isset( $token ) && $request_platform === 'wordpress' ) {
+		if ( isset( $token ) && 'wordpress' === $request_platform ) {
 			return true;
 		} else {
 			return new WP_Error( 'rest_forbidden', esc_html__( 'Invalid Authorization.', 'gdpr-cookie-consent' ), array( 'status' => 401 ) );
