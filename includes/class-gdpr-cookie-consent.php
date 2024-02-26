a5d57bac5be3cda888a1e587f5a2296c0886797e
OjasA21 <ojas.awachat@hbwsl.com>
2024-02-26 09:54:36 +0530

    Change default setting of data-request

--- includes/class-gdpr-cookie-consent.php
+++ includes/class-gdpr-cookie-consent.php
@@ -239,12 +239,11 @@ class Gdpr_Cookie_Consent {
 				// create table in db.
 				$this->loader->add_action( 'activated_plugin', $plugin_admin, 'update_db_check' );
 				$this->loader->add_action( 'plugins_loaded', $plugin_admin, 'update_db_check' );
-	
+
 				$this->loader->add_filter( 'gdpr_get_maxmind_integrated', $plugin_admin, 'wpl_get_maxmind_integrated' );
 				$this->loader->add_action( 'wp_ajax_wpl_cookie_consent_integrations_settings', $plugin_admin, 'wpl_cookie_consent_integrations_settings' );
 				$this->loader->add_action( 'admin_notices', $plugin_admin, 'wpl_admin_notices' );
 			}
-		
 		}
 	}
 
@@ -269,14 +268,14 @@ class Gdpr_Cookie_Consent {
 			$this->loader->add_action( 'wp_footer', $plugin_public, 'gdprcookieconsent_inject_gdpr_script' );
 			// added rest endpoint for fetching current options for banner.
 			$this->loader->add_action( 'rest_api_init', $plugin_public, 'gdpr_cookie_data_endpoint' );
-			if(!get_option( 'wpl_pro_active' )){
-			// action hooks for renew consnet.
-			$this->loader->add_action( 'wp_ajax_nopriv_gdpr_renew_consent_bar', $plugin_public, 'gdpr_renew_consent_bar' );
-			$this->loader->add_action( 'wp_ajax_gdpr_renew_consent_bar', $plugin_public, 'gdpr_renew_consent_bar' );
-			//action hooks for geo integration.
-			$this->loader->add_action( 'wp_ajax_nopriv_show_cookie_consent_bar', $plugin_public, 'show_cookie_consent_bar' );
-			$this->loader->add_action( 'wp_ajax_show_cookie_consent_bar', $plugin_public, 'show_cookie_consent_bar' );
-			$this->loader->add_filter( 'gdprcookieconsent_json_settings', $plugin_public, 'wplcookieconsent_json_settings', 10, 1 );
+			if ( ! get_option( 'wpl_pro_active' ) ) {
+				// action hooks for renew consnet.
+				$this->loader->add_action( 'wp_ajax_nopriv_gdpr_renew_consent_bar', $plugin_public, 'gdpr_renew_consent_bar' );
+				$this->loader->add_action( 'wp_ajax_gdpr_renew_consent_bar', $plugin_public, 'gdpr_renew_consent_bar' );
+				// action hooks for geo integration.
+				$this->loader->add_action( 'wp_ajax_nopriv_show_cookie_consent_bar', $plugin_public, 'show_cookie_consent_bar' );
+				$this->loader->add_action( 'wp_ajax_show_cookie_consent_bar', $plugin_public, 'show_cookie_consent_bar' );
+				$this->loader->add_filter( 'gdprcookieconsent_json_settings', $plugin_public, 'wplcookieconsent_json_settings', 10, 1 );
 			}
 		}
 	}
