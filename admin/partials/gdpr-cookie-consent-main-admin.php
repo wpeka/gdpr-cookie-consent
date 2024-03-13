6dff8cbe0092359259f45894345517331df20ef2
Ujjawal <ujjawal.chhetri@hbwsl.com>
2024-03-13 20:04:24 +0530

    move activation page to admin tab

--- admin/partials/gdpr-cookie-consent-main-admin.php
+++ admin/partials/gdpr-cookie-consent-main-admin.php
@@ -126,6 +126,7 @@ $is_user_connected = $this->settings->is_connected();
 						<div class="gdpr-cookie-consent-admin-tab gdpr-cookie-consent-admin-integrations-data-tab" data-tab="integrations">
 							<p class="gdpr-cookie-consent-admin-tab-name">Integrations</p>
 						</div>
+				
 				<?php
 
 
@@ -168,6 +169,16 @@ $is_user_connected = $this->settings->is_connected();
 				<!-- <div class="gdpr-cookie-consent-admin-tab gdpr-cookie-consent-admin-legalpage-data-tab" data-tab="legal_page">
 					<p class="gdpr-cookie-consent-admin-tab-name">Legal&nbsp;Page</p>
 				</div> -->
+				<?php 
+				if($pro_is_activated){
+					?>
+					<!-- Pro activation key -->
+				        <div class="gdpr-cookie-consent-admin-tab gdpr-cookie-consent-admin-pro-activation-tab" data-tab="activation_key">
+							<p class="gdpr-cookie-consent-admin-tab-name">Pro Activation</p>
+						</div>
+				<?php }
+				
+				?>
 			</div>
 		</div>
 
@@ -206,6 +217,11 @@ $is_user_connected = $this->settings->is_connected();
 				<!-- data req data content  -->
 				<div class="gdpr-cookie-consent-admin-data-request-data-content gdpr-cookie-consent-admin-tab-content" id="data_request">
 					<?php do_action( 'add_data_request_content' ); ?>
+					<?php require_once plugin_dir_path( __FILE__ ) . 'gdpr-data-request-tab-template.php'; ?>
+				</div>
+				<!-- data req data content  -->
+				<div class="gdpr-cookie-consent-admin-data-request-activation-key gdpr-cookie-consent-admin-tab-content" id="activation_key">
+					<?php do_action( 'add_activation_key_content' ); ?>
 					<?php require_once plugin_dir_path( __FILE__ ) . 'gdpr-data-request-tab-template.php'; ?>
 				</div>
 				<!-- legal pages  -->
