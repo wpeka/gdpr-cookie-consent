9e974e9f0f1e05cb45f5209f8747189ee67d08cb
OjasA21 <ojas.awachat@hbwsl.com>
2024-04-19 17:01:54 +0530

    Added internationalization of hardcoded string

--- admin/partials/gdpr-cookie-consent-main-admin.php
+++ admin/partials/gdpr-cookie-consent-main-admin.php
@@ -79,22 +79,22 @@ $is_user_connected = $this->settings->is_connected();
 			<div class="gdpr-api-info-content">
 			<div class="gdpr-api-detailed-info">
 			<h2>
-				<?php echo esc_html( 'Connect your website to WP Cookie Consent' ); ?>
+				<?php echo esc_html( 'Connect your website to WP Cookie Consent', 'gdpr-cookie-consent' ); ?>
 			</h2>
-			<p><?php echo esc_html( 'Sign up for a free account to integrate seamlessly with the WP Cookie Consent server. Once connected, gain full control over your settings and unlock advanced features:' ); ?></p>
+			<p><?php echo esc_html( 'Sign up for a free account to integrate seamlessly with the WP Cookie Consent server. Once connected, gain full control over your settings and unlock advanced features:' , 'gdpr-cookie-consent'); ?></p>
 			<p>
-				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark' ); ?>"></span> <strong><?php echo esc_html( 'Cookie Scanner:' ); ?></strong> <?php echo esc_html( 'Identify cookies on your website and automatically block them before user consent (essential for legal compliance).' ); ?>
+				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark', 'gdpr-cookie-consent' ); ?>"></span> <strong><?php echo esc_html( 'Cookie Scanner:' , 'gdpr-cookie-consent'); ?></strong> <?php echo esc_html( 'Identify cookies on your website and automatically block them before user consent (essential for legal compliance).', 'gdpr-cookie-consent' ); ?>
 			</p>
 			<p>
-				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark' ); ?>"></span> <strong><?php echo esc_html( 'Advanced Dashboard:' ); ?></strong> <?php echo esc_html( 'Unlock useful insights on user\'s consent data, cookie summary, and consent logs.' ); ?>
+				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark', 'gdpr-cookie-consent' ); ?>"></span> <strong><?php echo esc_html( 'Advanced Dashboard:', 'gdpr-cookie-consent' ); ?></strong> <?php echo esc_html( 'Unlock useful insights on user\'s consent data, cookie summary, and consent logs.', 'gdpr-cookie-consent' ); ?>
 			</p>
 			<p>
-				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark' ); ?>"></span> <strong><?php echo esc_html( 'Geo-targeting:' ); ?></strong> <?php echo esc_html( 'Display or hide the GDPR cookie consent notice depending on the visitor’s location.' ); ?>
+				<span><img src="<?php echo esc_url( GDPR_COOKIE_CONSENT_PLUGIN_URL ) . 'admin/images/bullet_point.png'; ?>" alt="<?php echo esc_attr( 'API Connection Success Mark', 'gdpr-cookie-consent' ); ?>"></span> <strong><?php echo esc_html( 'Geo-targeting:', 'gdpr-cookie-consent' ); ?></strong> <?php echo esc_html( 'Display or hide the GDPR cookie consent notice depending on the visitor’s location.' , 'gdpr-cookie-consent'); ?>
 			</p>
 		</div>
 		<div class="gdpr-api-connection-btns">
-			<button class="gdpr-start-auth"><?php echo esc_html( 'New? Create a free account' ); ?></button>
-			<button class="api-connect-to-account-btn"><?php echo esc_html( 'Connect your existing account' ); ?></button>
+			<button class="gdpr-start-auth"><?php echo esc_html( 'New? Create a free account', 'gdpr-cookie-consent' ); ?></button>
+			<button class="api-connect-to-account-btn"><?php echo esc_html( 'Connect your existing account', 'gdpr-cookie-consent' ); ?></button>
 		</div>
 
 
