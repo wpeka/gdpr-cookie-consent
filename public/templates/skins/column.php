316eeca5a895c0490845da2b990ed83337c5db64
Shantanoo-HBWSL <shantanoo.chandorkar@hbwsl.com>
2024-10-21 17:04:21 +0530

    Fixed 'Read More' visibility bug on CCPA Banner when GDPA & CCPA both are selected.

--- public/templates/skins/column.php
+++ public/templates/skins/column.php
@@ -12,54 +12,51 @@ if ( ! empty( $the_options['gdpr_notify'] ) ) {
 	$ab_options = get_option('wpl_ab_options');
 	?>
 	<div class="gdpr_messagebar_content">
-		<div class="group-description" tabindex="0">
 		<div class="gdpr_logo_container">
-		<?php
-			$get_banner_img = get_option( GDPR_COOKIE_CONSENT_SETTINGS_LOGO_IMAGE_FIELD );
-		if ( ! empty( $get_banner_img ) ) {
-			?>
-			<img class="gdpr_logo_image" alt="logo-image" src="<?php echo esc_url_raw( $get_banner_img ); ?>" >
-				<?php
-		}
-		?>
-		</div>
 			<?php
+					$get_banner_img = get_option( GDPR_COOKIE_CONSENT_SETTINGS_LOGO_IMAGE_FIELD );
+					if ( ! empty( $get_banner_img ) ) {
+						?>
+					<img class="gdpr_logo_image" alt="logo-image" src="<?php echo esc_url_raw( $get_banner_img ); ?>" >
+					<?php
+				}
+				?>
+		</div>
+		<?php
 			if ( '' !== $the_options['head'] ) {
 				?>
 				<h3 class="gdpr_messagebar_head">
-				<?php
+					<?php
 				echo esc_html__( $the_options['head'], 'gdpr-cookie-consent' ); //phpcs:ignore
 				?>
 	</h3>
+	<?php
+			}
+			?>
+			<div class="group-description" tabindex="0">
+		<p class="gdpr"><?php echo wp_kses_post( $the_options['gdpr_str'], '<a><br><em><strong><span><p><i><img><b><div><label>' ); ?>
+		<?php
+		if ( ! empty( $the_options['button_readmore_is_on'] ) ) {
+			?>
+			<a id="cookie_action_link" href="<?php echo esc_html( $the_options['button_readmore_url_link'] ); ?>" class="<?php echo esc_html( $the_options['button_readmore_classes'] ); ?>"
+			<?php
+			if ( ! empty( $the_options['button_readmore_new_win'] ) ) {
+				?>
+				target="_blank"
 				<?php
 			}
 			?>
-			<p class="gdpr"><?php echo wp_kses_post( $the_options['gdpr_str'], '<a><br><em><strong><span><p><i><img><b><div><label>' ); ?></p>
-			
+	><?php echo esc_html__( $the_options['button_readmore_text'], 'gdpr-cookie-consent' ); //phpcs:ignore ?></a>
 			<?php
-			if ( ! empty( $the_options['button_readmore_is_on'] ) ) {
-				?>
-				<p>
-					<a id="cookie_action_link" href="<?php echo esc_html( $the_options['button_readmore_url_link'] ); ?>" class="<?php echo esc_html( $the_options['button_readmore_classes'] ); ?>"
-						<?php
-						if ( ! empty( $the_options['button_readmore_new_win'] ) ) {
-							?>
-							target="_blank"
-							<?php
-						}
-						?>
-					>
-					<?php
-					echo esc_html__( $the_options['button_readmore_text'], 'gdpr-cookie-consent' ); // phpcs:ignore
-					?>
-	</a>
-				</p>
-				<?php
-			}
+		}
+		?>
+			</p>
+			<?php
 			if ( ! empty( $the_options['ccpa_notify'] ) ) {
 				?>
 				<?php 
-		if($the_options['cookie_usage_for'] == 'both') { ?>
+		if($the_options['cookie_usage_for'] == 'both') { 
+			?>
 			<p class="ccpa"><?php echo wp_kses_post( $the_options['ccpa_str'], '<a><br><em><strong><span><p><i><img><b><div><label>' ); ?>
 			<?php
 			if ( ! empty( $the_options['button_donotsell_is_on'] ) ) {
@@ -71,7 +68,7 @@ if ( ! empty( $the_options['gdpr_notify'] ) ) {
 			?>
 		</p>
 		<?php } else { ?>
-			<p class="ccpa"><?php echo wp_kses_post( $the_options['ccpa_str'], '<a><br><em><strong><span><p><i><img><b><div><label>' ); ?>	
+			<p class="ccpa"><?php echo wp_kses_post( $the_options['ccpa_str'], '<a><br><em><strong><span><p><i><img><b><div><label>' ); ?>
 			<?php
 			if ( ! empty( $the_options['button_donotsell_is_on'] ) ) {
 				?>
