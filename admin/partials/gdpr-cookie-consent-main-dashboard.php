b82d82637522500014ff40964a9184a4fe617c90
tejas <tejas.sonawane@hbwsl.com>
2025-03-05 22:48:31 +0530

    Fix incorrect popup pops out on clicking login button

--- admin/partials/gdpr-cookie-consent-main-dashboard.php
+++ admin/partials/gdpr-cookie-consent-main-dashboard.php
@@ -148,8 +148,8 @@ $remaining_percentage_scan_limit = round( ( get_option( 'gdpr_no_of_page_scan' )
 						</p>
 					</div>
 				<div class="gdpr-api-connection-btns">
-					<button class="gdpr-start-auth"><?php echo esc_html( 'Sign Up for Free', 'gdpr-cookie-consent' ); ?></button>
-					<p><?php echo esc_html( 'Already have an account?', 'gdpr-cookie-consent' ); ?><a class="gdpr-start-auth" href=""><?php esc_html_e( 'Connect your existing account', 'gdpr-cookie-consent' ); ?></a></p>
+					<button class="gdpr-start-auth gdpr-signup"><?php echo esc_html( 'Sign Up for Free', 'gdpr-cookie-consent' ); ?></button>
+					<p><?php echo esc_html( 'Already have an account?', 'gdpr-cookie-consent' ); ?><a class="gdpr-start-auth gdpr-login" href=""><?php esc_html_e( 'Connect your existing account', 'gdpr-cookie-consent' ); ?></a></p>
 				</div>
 			</div>
 			
