32b6302155909e0ba8fc67e8a648e1627a5cca41
Neelaksh Singh <neelaksh.singh@hbwsl.com>
2025-12-03 10:43:07 +0530

    url fix

--- admin/class-gdpr-cookie-consent-admin.php
+++ admin/class-gdpr-cookie-consent-admin.php
@@ -8320,7 +8320,7 @@ class Gdpr_Cookie_Consent_Admin {
 
 		// Add our own permissive CORS headers
 		add_filter( 'rest_pre_serve_request', function( $value ) {
-			header( 'Access-Control-Allow-Origin: *' );
+			header( 'Access-Control-Allow-Origin: https://app.wplegalpages.com' );
 			header( 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS' );
 			header( 'Access-Control-Allow-Credentials: true' );
 			header( 'Access-Control-Allow-Headers: Authorization, Content-Type, X-WP-Nonce, Origin, X-Requested-With, Accept' );
